8b9ff762ae840034b7edeab09ffc5eca35a9bb63
Cristian Gonzalez <cristian.glez.m@gmail.com>
2024-01-10 00:12:48 +0100

    wait to publish to npm before publishing to github pkg

--- .devops/publish.sh
+++ .devops/publish.sh
@@ -9,6 +9,7 @@ cd dist
 echo "@cristianglezm:registry:https://registry.npmjs.org" > .npmrc
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
 npm publish --access public
+sleep 10s
 echo "@cristianglezm:registry:https://npm.pkg.github.com" > .npmrc
 echo "//npm.pkg.github.com/:_authToken=${GH_TOKEN}" >> .npmrc
 npm publish --access public
