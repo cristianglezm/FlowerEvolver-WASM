a8d9403cd0d42d25126e638b5a33e997bd4ee8e2
Cristian Gonzalez <cristian.glez.m@gmail.com>
2026-09-16 16:12:13 +0200

    fix(ci): add provenance to npm
    * bump version to 4.0.1
    * fix find{JsonBox, EvoAI}.cmake
    * add missing python cp314-win_amd64 for windows build

--- .devops/publish.sh
+++ .devops/publish.sh
@@ -7,12 +7,12 @@ GH_TOKEN=$1
 cd dist
 
 echo "@cristianglezm:registry=https://registry.npmjs.org" > .npmrc
-npm publish --access public
+npm publish --access public --provenance
 
 sleep 10s
 
 echo "@cristianglezm:registry=https://npm.pkg.github.com" > .npmrc
 echo "//npm.pkg.github.com/:_authToken=\${GH_TOKEN}" >> .npmrc
-npm publish --access public
+npm publish --access public --provenance
 
 cd ..
